61c6b8be59d710312ff12a05f45a91b917b2de85
meshi <mehhi@yandex.ru>
2022-10-22 14:02:12 +0300

    icon

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -4,11 +4,11 @@ import cl from './Column.module.css';
 function Column({id, color, isLocked, setColumnColor}) {
     return (
     <div className={cl.column} style={{backgroundColor: color}}>
-        <button onClick={() => setColumnColor(id)}>
-            <h2>
+        {/* <button onClick={() => setColumnColor(id)}> */}
+            <h2 className={cl.colorTitle} onClick={() => setColumnColor(id)}>
                 {color}
             </h2>
-        </button>
+        {/* </button> */}
         <button>Locked: {String(isLocked)}</button>
     </div>
     );
